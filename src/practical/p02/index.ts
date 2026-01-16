0f7d2b17835fe909c4c20a3d413b59f83182660d
potcharaeiei <68010736@kmitl.ac.th>
2026-01-16 10:02:36 +0700

    sd

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,31 @@
-export function getPostsByUser() {}
+import axios from "axios";
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface PostResult {
+  id: number;
+  title: string;
+}
+
+async function getPostsByUser(userId: number): Promise<PostResult[]> {
+  try {
+    const response = await axios.get<Post[]>(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    return response.data
+      .filter((post) => post.userId === userId)   // ใช้ array method
+      .map((post) => ({
+        id: post.id,
+        title: post.title,
+      }));
+  } catch (error) {
+    return [];
+  }
+}
+
